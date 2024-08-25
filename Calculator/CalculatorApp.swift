7d16cafe53c523e9fa97479a30cd0c75441e0677
Jacob Wilson <62467882+Notwacob@users.noreply.github.com>
2024-08-24 20:58:21 -0400

    Added files via upload

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jacob Wilson on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
